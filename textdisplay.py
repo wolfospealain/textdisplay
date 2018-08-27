9b656705abaa21a20443db58c9331f849ec056c7
wolfospealain <eoinospealain@gmail.com>
2018-08-27 08:23:59 +0100

    Update textdisplay.py

--- textdisplay.py
+++ textdisplay.py
@@ -13,42 +13,44 @@ from tkinter import font
 class TextDisplay:
     """Creates a GUI screen to display the updated output of the text_source function, and repeat after delay."""
 
-    def __init__(self, text_source, delay=1000, title="", icon="", text_colour="lightgreen", background_colour="black",
-                 font_face="Courier", typing_mode=False, multi_line_wrap=False, font_size=0):
-        self.update = text_source
+    def __init__(self, command, delay=1000, title="", icon="", align="l", colour="lightgreen",
+                 background="black", font="Courier", typing_mode=False, wrap=False, size=0):
+        self.update = command
         self.delay = delay
         self.screen = Tk(className=title)
         self.screen.wm_iconphoto(True, PhotoImage(file=icon))
         self.screen.title(title)
-        self.text_colour = text_colour
-        self.background_colour = background_colour
-        self.font_face = font_face
+        self.align = align[0]
+        self.colour = colour
+        self.background = background
+        self.font = font
         self.typing_mode = typing_mode
-        self.multi_line_wrap = multi_line_wrap if not self.typing_mode else True
-        self.screen.configure(background=self.background_colour)
+        self.wrap = wrap if not self.typing_mode else True
+        self.screen.configure(background=self.background)
         self.screen.resizable(width=YES, height=YES)
         self.screen.attributes("-fullscreen", True)
         self.screen.bind("<F11>", self.toggle_fullscreen)
-        self.screen.bind("<Escape>", self.end_fullscreen)
-        self.screen.bind("<Control-c>", self.quit)
+        self.screen.bind("<Escape>", self.quit)
         self.text = ""
-        if font_size != 0 and not self.typing_mode:
-            self.font_size = font_size
+        if size != 0 and not self.typing_mode:
+            self.size = size
             self.auto_font_size = False
         else:
-            self.font_size = 12
+            self.size = 12
             self.auto_font_size = True
-        self.textbox = Text(self.screen, font=(self.font_face, font_size, 'bold'), bg=self.background_colour,
-                            fg=self.text_colour if self.typing_mode else self.background_colour, border=0, relief=FLAT,
-                            highlightbackground=self.background_colour, wrap=WORD if self.multi_line_wrap else NONE)
-        self.textbox.pack(expand=True, fill="both", padx=50, pady=50)
+        self.textbox = Text(self.screen, font=(self.font, size, 'bold'), bg=background,
+                            fg=self.colour if self.typing_mode else self.background, border=0, relief=FLAT,
+                            highlightbackground=self.background, wrap=WORD if self.wrap else NONE)
+        self.textbox.pack(expand=True, fill=BOTH, padx=50, pady=50)
         if self.typing_mode:
-            self.textbox.configure(insertbackground=self.text_colour)
+            self.textbox.configure(insertbackground=self.colour)
             self.textbox.bind("<F5>", self.textbox_refresh)
             self.textbox.bind("<Key>", self.textbox_focus)
             self.textbox.bind("<1>", self.textbox_focus)
             self.textbox.delete("1.0", END)
-            self.textbox.insert(INSERT, self.text)
+            self.textbox.insert(INSERT, self.text, "text")
+            self.textbox.tag_config("text", justify=self.align)
+            self.textbox.focus_set()
         else:
             self.screen.after(self.delay, self.tick, self.delay)
         self.screen.mainloop()
@@ -57,7 +59,7 @@ class TextDisplay:
         exit()
 
     def textbox_focus(self, event):
-        self.textbox.configure(insertbackground=self.text_colour)
+        self.textbox.configure(insertbackground=self.colour)
 
     def textbox_refresh(self, event):
         self.tick(False)
@@ -69,69 +71,65 @@ class TextDisplay:
         self.screen.attributes("-fullscreen", False)
 
     def visibility(self, textbox, wrappable=True):
-        """Return the visibility of rows and columns in a tkinter Text widget: false if cropped."""
-        character_font = font.Font(family=self.font_face, size=self.font_size, weight='bold')
-        line_height = character_font.metrics('linespace')
+        """Return the visibility of in a tkinter Text widget: false if cropped."""
+        character_font = font.Font(family=self.font, size=self.size, weight='bold')
         text = textbox.get("1.0", END).split("\n")[:-1]
-        lines = len(text)
-        visible_rows = False
-        visible_columns = False
-        # scrollable, with no line wrapping
         if not wrappable:
             widest_line = 0
-            for line in range(0, lines):
+            for line in range(0, len(text)):
                 if len(text[line]) > len(text[widest_line]):
                     widest_line = line
-            if character_font.measure(text[widest_line]) <= textbox.winfo_width():
-                visible_columns = True
+            visible = character_font.measure(text[widest_line]) <= textbox.winfo_width()
         else:
-            # check first character visibility for every line (including line height)
-            for line in range(1, lines + 1):
-                bounding_box = textbox.bbox("%d.%d" % (line, 0))
-                if bounding_box and bounding_box[3] >= line_height:
-                    visible_rows = True
-                    # check last character visibility (including line height and font width)
-                    character = len(text[line - 1]) - 1
-                    if character > -1:
-                        bounding_box = textbox.bbox("%d.%d" % (line, character))
-                        if bounding_box and bounding_box[2] >= character_font.measure(text[line - 1][character]) and \
-                                bounding_box[3] >= line_height:
-                            visible_columns = True
-                        else:
-                            visible_columns = False
-                elif visible_rows:
-                    visible_rows = False
-                    break
-                if visible_rows and not visible_columns:
-                    break
-        return visible_rows, visible_columns
+            bounding_box = textbox.bbox("end-1c")
+            visible = bounding_box and bounding_box[3] >= character_font.metrics('linespace')
+        return visible
 
     def tick(self, delay=False):
         if not self.typing_mode:
-            text = self.update().strip()
+            text, options = self.update()
+            text = text.strip()
+            if options:
+                if "delay" in options:
+                    delay = options["delay"]
+                if "wrap" in options:
+                    self.wrap = options["wrap"]
+                    self.textbox.config(wrap=WORD if self.wrap else NONE)
+                if "align" in options:
+                    self.align = options["align"]
+                if "text_colour" in options:
+                    self.colour = options["text_colour"]
+                if "font" in options:
+                    self.font = options["font"]
+                    self.textbox.config(font=(self.font, self.size, "bold"))
             if text:
                 self.text = text
-                self.textbox.delete("1.0", END)
-                self.textbox.insert(INSERT, self.text)
+            self.textbox.delete("1.0", END)
+            self.textbox.insert(INSERT, self.text, "text")
+            self.textbox.tag_config("text", justify=self.align)
         else:
-            self.textbox.configure(insertbackground=self.background_colour)
-        if self.multi_line_wrap:
+            self.textbox.config(insertbackground=self.background)
+        if self.wrap:
             self.textbox.see("1.0")
         if self.auto_font_size:
-            row, col = self.visibility(self.textbox, self.multi_line_wrap)
-            if (not self.multi_line_wrap or row) and col:
-                while (not self.multi_line_wrap or row) and col:
-                    self.font_size += 1
-                    self.textbox.config(font=(self.font_face, self.font_size, "bold"))
-                    row, col = self.visibility(self.textbox, self.multi_line_wrap)
-                self.font_size -= 1
-                self.textbox.config(font=(self.font_face, self.font_size, "bold"))
+            # find maximum font size
+            if self.visibility(self.textbox, self.wrap):
+                while self.visibility(self.textbox, self.wrap):
+                    self.size += 1
+                    self.textbox.config(font=(self.font, self.size, "bold"))
+                self.size -= 1
+                self.textbox.config(font=(self.font, self.size, "bold"))
             else:
-                while ((self.multi_line_wrap and not row) or not col) and self.font_size > 1:
-                    self.font_size -= 1
-                    self.textbox.config(font=(self.font_face, self.font_size, "bold"))
-                    row, col = self.visibility(self.textbox, self.multi_line_wrap)
-        self.textbox.config(fg=self.text_colour)
+                while not self.visibility(self.textbox, self.wrap) and self.size > 1:
+                    self.size -= 1
+                    self.textbox.config(font=(self.font, self.size, "bold"))
+        # vertically centre text
+        if self.visibility(self.textbox, wrappable=True) == 1:
+            space = self.textbox.winfo_height() - (self.textbox.bbox("end-1c")[1] + self.textbox.bbox("end-1c")[3])
+            line_height = font.Font(family=self.font, size=self.size, weight='bold').metrics('linespace')
+            lines = int(space / line_height / 2)
+            self.textbox.insert("1.0", "\n" * lines)
+        self.textbox.config(fg=self.colour)
         self.screen.update_idletasks()
         if delay:
             self.screen.after(delay, self.tick, delay)
@@ -161,12 +159,15 @@ if __name__ == "__main__":
             return False
 
 
-    def parse_command_line(description):
-        parser = argparse.ArgumentParser(description="%(prog)s " + description,
-                                         epilog="CTRL-C to exit. ESC or F11 to toggle fullscreen.")
+    def parse_command_line():
+        parser = argparse.ArgumentParser(
+            description="%(prog)s " + "Python module (and standalone command-line tool) to easily pipe information to a large text fullscreen/windowed GUI. https://github.com/wolfospealain/textdisplay",
+            epilog="ESC to exit, or F11 to toggle fullscreen.")
         if ".py" in sys.argv[0]:
             parser.add_argument("--install", action="store_true", dest="install", default=False,
-                                help="install to Linux destination path (default: " + install_path + ")")
+                                help="install to Linux /usr/local/bin")
+        parser.add_argument("-a", "--align", action="store", dest="align", default="left",
+                            help="align text horizontally: l/left, r/right, c/centre")
         parser.add_argument("-b", "--background", action="store", dest="background", default="black",
                             help="background colour")
         parser.add_argument("-c", "--colour", action="store", dest="colour", default="lightgreen",
@@ -177,12 +178,12 @@ if __name__ == "__main__":
                             help="font (see https://tkdocs.com/tutorial/fonts.html)")
         parser.add_argument("-l", "--lines", action="store", dest="lines", type=int, default=0,
                             help="number of lines to display (default: all)")
-        parser.add_argument("-p", "--page", action="store_true", dest="page", default=False,
-                            help="fill page, multi-line wrap")
         parser.add_argument("-s", "--font-size", action="store", dest="size", type=int, default=0,
                             help="font size (default: auto)")
         parser.add_argument("-t", "--typing-mode", action="store_true", dest="typing", default=False,
                             help="direct entry mode (F5 to automatically resize text)")
+        parser.add_argument("-w", "--wrap", action="store_true", dest="wrap", default=False,
+                            help="line wrap, single page")
         args = parser.parse_args()
         return args
 
@@ -198,18 +199,15 @@ if __name__ == "__main__":
             for line in range(0, args.lines - 1):
                 text += sys.stdin.readline()
             text += sys.stdin.readline().strip()
-        return text
+        return (text, None)
 
 
-    install_path = "/usr/local/bin"
-    name = "textdisplay"
-    description = "Python module (and standalone command-line tool) to easily pipe information to a large text fullscreen/windowed GUI. https://github.com/wolfospealain/textdisplay"
-
-    args = parse_command_line(description)
+    args = parse_command_line()
     if ".py" in sys.argv[0]:
         if args.install:
-            install(install_path, name)
+            install("/usr/local/bin", "textdisplay")
             exit()
-    display = TextDisplay(text_source, title=sys.argv[0], text_colour=args.colour, background_colour=args.background,
-                          font_face=args.font, font_size=args.size, delay=args.delay, typing_mode=args.typing,
-                          multi_line_wrap=args.page, icon="/usr/share/icons/gnome/256x256/apps/utilities-terminal.png")
+    display = TextDisplay(text_source, title=sys.argv[0], align=args.align, colour=args.colour,
+                          background=args.background,
+                          font=args.font, size=args.size, delay=args.delay, typing_mode=args.typing,
+                          wrap=args.wrap, icon="/usr/share/icons/gnome/256x256/apps/utilities-terminal.png")
